82fe0fb8831517510891a991add96bcbaedd7fdb
Tim Fischer <fischeti@iis.ee.ethz.ch>
2023-09-01 08:44:43 +0200

    sw: Add sanity tests

---
+++ target/sim/sw/host/apps/sanity/src/sanity.c
@@ -0,0 +1,9 @@
+// Copyright 2022 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "host.c"
+
+int main() {
+    return 0;
+}
