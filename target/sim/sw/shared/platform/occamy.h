47d4c20c05cc63b47bdeee6c57b40beeaca23c20
Luca Colagrande <luca.colagrande3@gmail.com>
2023-09-15 10:18:23 +0200

    sw: Add `N_CLUSTERS_TO_USE` parameter
    `N_CLUSTERS_TO_USE` overrides `SNRT_NUM_CLUSTERS` to ensure that
    global synchronization still works despite a subset of clusters
    is woken up.

--- target/sim/sw/shared/platform/occamy.h
+++ target/sim/sw/shared/platform/occamy.h
@@ -5,6 +5,10 @@
 #include "occamy_cfg.h"
 #include "occamy_memory_map.h"
 
+#ifdef N_CLUSTERS_TO_USE
+#define N_CLUSTERS N_CLUSTERS_TO_USE
+#else
 #define N_CLUSTERS (N_QUADS * N_CLUSTERS_PER_QUAD)
+#endif
 #define N_SNITCHES (N_CLUSTERS * N_CORES_PER_CLUSTER)
 #define N_HARTS (N_SNITCHES + 1)
