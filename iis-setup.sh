4606d4307c531205dfa9fe0731cb1cfc27a79ffc
Luca Colagrande <luca.colagrande3@gmail.com>
2024-10-03 22:09:13 +0200

    treewide: Remove verible

--- iis-setup.sh
+++ iis-setup.sh
@@ -13,12 +13,3 @@ cd -
 # Define required environment variables
 export CLANG_FORMAT=clang-format-10.0.1
 export HOST_RISCV_GCC_BINROOT=/usr/pack/riscv-1.0-kgf/riscv64-gcc-12.2.0/bin
-
-# Install verible
-mkdir -p tools/verible
-chmod 777 tools/verible
-cd tools/verible
-curl -Ls -o verible.tar.gz https://github.com/chipsalliance/verible/releases/download/v0.0-3222-gb19cdf44/verible-v0.0-3222-gb19cdf44-CentOS-7.9.2009-Core-x86_64.tar.gz
-tar -xzf verible.tar.gz --strip-components=1
-export PATH=$(pwd)/bin:$PATH
-cd -
